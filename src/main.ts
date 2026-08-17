8e7e22207d6ef6bb97d7f5f3a8f15ca7ceadbd05
Mishra-coder <devendramishra495@gmail.com>
2026-08-17 18:16:57 +0530

    Update Electron main process window lifecycle and asset protocol handlers

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,7 @@ function createWindow(): void {
 
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     win.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
